6f8054cc9395de4c2af0bf7e119b4de4d66cf4ae
Your Name <joey.yashinsky@gmail.com>
2021-02-28 18:25:16 -0500

    remove 'hidden' from modal and overlay so modal window will now appear

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -7,7 +7,9 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', function () {
-        console.log('Button Clicked');
-    })
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
