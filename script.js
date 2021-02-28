9152fb2fcccbe7fc941e9fcf17a9b29c3233595b
Your Name <joey.yashinsky@gmail.com>
2021-02-28 02:41:47 -0500

    select and create variables for modal, overlay, and close-modal

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector('.close-modal');
