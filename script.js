6315263a73b4a491bcf06dc8444bdb4c3ef3ca6e
Your Name <joey.yashinsky@gmail.com>
2021-03-01 10:13:00 -0500

    keypress event added and working. modal now closes only if Esc key is pressed. If any other key is pressed, nothing happens.

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-    console.log('Button Clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
@@ -23,5 +22,7 @@ btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-    console.log(e);
-})
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
